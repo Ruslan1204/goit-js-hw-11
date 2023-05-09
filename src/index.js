45f2d7a6e0e01840bb8c0ab01718387c5277eda3
Ruslan <113464399+Ruslan1204@users.noreply.github.com>
2023-05-09 18:12:08 +0300

    s

--- src/index.js
+++ src/index.js
@@ -24,42 +24,24 @@ refs.button.setAttribute('hidden', false);
 refs.form.addEventListener('submit', onForm);
 refs.button.addEventListener('click', onClick);
 
+function formScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
 async function getUser(name, pages = 1) {
   try {
     const response = await axios.get(
-      `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${pages}&per_page=100`
+      `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${pages}&per_page=40`
     );
     return response;
   } catch (error) {
     console.log(error);
-  }
-}
-
-function onForm(evt) {
-  evt.preventDefault();
-  name = refs.input.value;
-  const trim = name.trim();
-
-  if (!name) {
-    Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-    return;
-  }
-
-  if (!trim) {
-    return;
-  } else {
-    getUser(name, (pages = 1)).then(function (response) {
-      creatMarkup(response.data.hits);
-      arrForm = response.data.hits.length;
-    });
-
-    setTimeout(() => {
-      refs.button.removeAttribute('hidden');
-    }, 500);
-
-    return;
   }
 }
 
@@ -70,38 +52,72 @@ function creatMarkup(arr) {
       return `<div class="photo-card">
         <div class="gallery__item">
         <a href="${item.largeImageURL}">
-        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width='300'/>
+        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width='300' height="200"/>
         </a>
         </div>
         <div class="info">
           <p class="info-item">
-            <b>Likes ${item.likes}</b>
+            <b>Likes: ${item.likes}</b>
           </p>
           <p class="info-item">
-            <b>Views ${item.views}</b>
+            <b>Views: ${item.views}</b>
           </p>
           <p class="info-item">
-            <b>Comments ${item.comments}</b>
+            <b>Comments: ${item.comments}</b>
           </p>
           <p class="info-item">
-            <b>Downloads ${item.downloads}</b>
+            <b>Downloads: ${item.downloads}</b>
           </p>
         </div>
       </div>`;
     })
     .join(' ');
 
+  console.log();
+
+  refs.div.insertAdjacentHTML('beforeend', markup);
+
   if (!arr.length) {
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
+  }
+
+  new SimpleLightbox('.gallery__item a').refresh();
+}
+
+function onForm(evt) {
+  evt.preventDefault();
+  name = refs.input.value;
+  const trim = name.trim();
+  refs.button.setAttribute('hidden', false);
+  if (!name) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+
     return;
   }
 
-  refs.div.insertAdjacentHTML('beforeend', markup);
-  // refs.div.innerHTML = markup;
+  if (!trim) {
+    return;
+  } else {
+    refs.div.innerHTML = '';
+  }
 
-  new SimpleLightbox('.gallery__item a');
+  getUser(name, (pages = 1)).then(function (response) {
+    creatMarkup(response.data.hits);
+    arrForm = response.data.hits.length;
+
+    if (!arrForm) {
+      refs.button.setAttribute('hidden', false);
+    } else {
+      refs.button.removeAttribute('hidden');
+
+      const totalHits = response.data.totalHits;
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
+  });
 }
 
 function onClick() {
@@ -111,10 +127,16 @@ function onClick() {
   getUser(name, pages).then(function (response) {
     creatMarkup(response.data.hits);
 
+    formScroll();
+
     arrClick = response.data.hits.length;
 
     if (arrLengthTotal === response.data.totalHits) {
       refs.button.setAttribute('hidden', false);
+
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
     }
   });
 }
